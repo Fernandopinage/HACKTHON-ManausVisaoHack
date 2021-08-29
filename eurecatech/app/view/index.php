000744744a49f96fbe2943b7b3c8fcc770f8ebac
FernandoPinage <luizfernandoluck@hotmail.com>
2021-08-29 15:07:21 -0400

    atualizando

--- eurecatech/app/view/index.php
+++ eurecatech/app/view/index.php
@@ -11,16 +11,18 @@ if (isset($_POST['pesquisar'])) {
 
 <!DOCTYPE html>
 <html lang="pt-br">
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/index.css">
-    <title>Eurecatech</title>
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/index.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<title>Eurecatech</title>
 </head>
 
 <body>
@@ -166,144 +168,162 @@ if (isset($_POST['pesquisar'])) {
                 <div class="form-check mb-2">
                     <input class="form-check-input" type="checkbox" value="" id="aaa" onclick="view2('aaa','lista2')">
                     <label class="form-check-label" for="aaa">
-                    10	SERVIÇOS DE INTERMEDIAÇÃO E CONGÊNERES
+                        10 SERVIÇOS DE INTERMEDIAÇÃO E CONGÊNERES
                     </label>
                 </div>
 
                 <div id="lista2" style="margin-left:20px; font-size:14px;" class="g-3">
-                <hr>
+                    <hr>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1007	AGENCIAMENTO DE NOTÍCIAS
+                            1007 AGENCIAMENTO DE NOTÍCIAS
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10071	 AGENCIAMENTO DE NOTÍCIAS
+                            10071 AGENCIAMENTO DE NOTÍCIAS
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10081	AGENCIAMENTO DE PUBLICIDADE E PROPAGANDA, INCLUSIVE O AGENCIAMENTO DE VEICULAÇÃO POR QUAISQUER MEIOS.
+                            10081 AGENCIAMENTO DE PUBLICIDADE E PROPAGANDA, INCLUSIVE O AGENCIAMENTO DE VEICULAÇÃO POR QUAISQUER MEIOS.
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1008	AGENCIAMENTO DE PUBLICIDADE E PROPAGANDA, INCLUSIVE O AGENCIAMENTO DE VEICULAÇÃO POR QUAISQUER MEIOS.
-                     </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
-                        <label class="form-check-label" for="7311400_0">
-                        1006	AGENCIAMENTO MARÍTIMO
+                            1008 AGENCIAMENTO DE PUBLICIDADE E PROPAGANDA, INCLUSIVE O AGENCIAMENTO DE VEICULAÇÃO POR QUAISQUER MEIOS.
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10061	AGENCIAMENTO MARÍTIMO
+                            1006 AGENCIAMENTO MARÍTIMO
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1005	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE BENS MÓVEIS OU IMÓVEIS, NÃO ABRANGIDOS EM OUTROS ITENS OU SUBITENS, INCLUSIVE AQUELES REALIZADOS NO ÂMBITO DE BOLSAS DE MERCADORIAS E FUTUROS, POR QUAISQUER MEIOS
+                            10061 AGENCIAMENTO MARÍTIMO
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10051	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE BENS MÓVEIS OU IMÓVEIS, NÃO ABRANGIDOS EM OUTROS ITENS OU SUBITENS, INCLUSIVE AQUELES REALIZADOS NO ÂMBITO DE BOLSAS DE MERCADORIAS E FUTUROS, POR QUAISQUER MEIOS
+                            1005 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE BENS MÓVEIS OU IMÓVEIS, NÃO ABRANGIDOS EM OUTROS ITENS OU SUBITENS, INCLUSIVE AQUELES REALIZADOS NO ÂMBITO DE BOLSAS DE MERCADORIAS E FUTUROS, POR QUAISQUER MEIOS
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1004	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE CONTRATOS DE ARRENDAMENTO MERCANTIL ( IEASING), DE FRANQUIA ( FRANCHISING ) E DE FATURIZAÇÃO ( FACTORING ).
-                        </label>
-                    </div>
-
-
-
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
-                        <label class="form-check-label" for="7311400_0">
-                        10041	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE CONTRATOS DE ARRENDAMENTO MERCANTIL ( IEASING 5,00), DE FRANQUIA ( FRANCHISING ) E DE FATURIZAÇÃO ( FACTORING ).
+                            10051 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE BENS MÓVEIS OU IMÓVEIS, NÃO ABRANGIDOS EM OUTROS ITENS OU SUBITENS, INCLUSIVE AQUELES REALIZADOS NO ÂMBITO DE BOLSAS DE MERCADORIAS E FUTUROS, POR QUAISQUER MEIOS
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10011	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE CÂMBIO E DE SEGUROS
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
-                        <label class="form-check-label" for="7311400_0">
-                       10031	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE DIREITOS DE PROPRIEDADE INDUSTRIAL, ARTÍSTICA OU LITERÁRIA.
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
-                        <label class="form-check-label" for="7311400_0">
-                       1003	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE DIREITOS DE PROPRIEDADE INDUSTRIAL, ARTÍSTICA OU LITERÁRIA.
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
-                        <label class="form-check-label" for="7311400_0">
-                        10012	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE PLANOS PRIVADOS DE ASSISTÊNCIA À SAÚDE E CONGÊNERES.
+                            1004 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE CONTRATOS DE ARRENDAMENTO MERCANTIL ( IEASING), DE FRANQUIA ( FRANCHISING ) E DE FATURIZAÇÃO ( FACTORING ).
                         </label>
                     </div>
 
+
+
+
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1002	 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE TÍTULOS EM GERAL, VALORES MOBILIÁRIOS E CONTRATOS QUAISQUER.
+                            10041 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE CONTRATOS DE ARRENDAMENTO MERCANTIL ( IEASING 5,00), DE FRANQUIA ( FRANCHISING ) E DE FATURIZAÇÃO ( FACTORING ).
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10021	AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE TÍTULOS EM GERAL, VALORES MOBILIÁRIOS E CONTRATOS QUAISQUER.
+                            10011 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE CÂMBIO E DE SEGUROS
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10101	 DISTRIBUIÇÃO DE BENS DE TERCEIROS
+                            10031 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE DIREITOS DE PROPRIEDADE INDUSTRIAL, ARTÍSTICA OU LITERÁRIA.
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1010	DISTRIBUIÇÃO DE BENS DE TERCEIROS.
+                            1003 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE DIREITOS DE PROPRIEDADE INDUSTRIAL, ARTÍSTICA OU LITERÁRIA.
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        10091	REPRESENTAÇÃO DE QUALQUER NATUREZA, INCLUSIVE COMERCIAL
+                            10012 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE PLANOS PRIVADOS DE ASSISTÊNCIA À SAÚDE E CONGÊNERES.
+                        </label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
+                        <label class="form-check-label" for="7311400_0">
+                            1002 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE TÍTULOS EM GERAL, VALORES MOBILIÁRIOS E CONTRATOS QUAISQUER.
                         </label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="7311400_0">
                         <label class="form-check-label" for="7311400_0">
-                        1009	REPRESENTAÇÃO DE QUALQUER NATUREZA, INCLUSIVE COMERCIAL
+                            10021 AGENCIAMENTO, CORRETAGEM OU INTERMEDIAÇÃO DE TÍTULOS EM GERAL, VALORES MOBILIÁRIOS E CONTRATOS QUAISQUER.
                         </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
+                        <label class="form-check-label" for="7311400_0">
+                            10101 DISTRIBUIÇÃO DE BENS DE TERCEIROS
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
+                        <label class="form-check-label" for="7311400_0">
+                            1010 DISTRIBUIÇÃO DE BENS DE TERCEIROS.
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
+                        <label class="form-check-label" for="7311400_0">
+                            10091 REPRESENTAÇÃO DE QUALQUER NATUREZA, INCLUSIVE COMERCIAL
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="7311400_0">
+                        <label class="form-check-label" for="7311400_0">
+                            1009 REPRESENTAÇÃO DE QUALQUER NATUREZA, INCLUSIVE COMERCIAL
+                        </label>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <hr>
+                    <div class="col text-end">
+                        <button type="button" class="btn btn-primary" id="liveToastBtn">CONCLUIR</button>
                     </div>
                 </div>
             </div>
 
+    </div>
 
 
-
-        </form>
+    </form>
+    <div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true" style="position: absolute; top: 5%; right: 1%;">
+        <div class="d-flex">
+            <div class="toast-body">
+                Você deseja realmente marca essa opção?
+                <hr>
+                Código proposto 
+            </div>
+                       
+  
+            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    </div>
     </div>
 
 
@@ -328,7 +348,7 @@ if (isset($_POST['pesquisar'])) {
 
         function view2(on, lista2) {
 
-           
+
             let checkbox = document.getElementById(on);
             if (checkbox.checked) {
                 document.getElementById(lista2).style.display = 'block';
@@ -338,13 +358,20 @@ if (isset($_POST['pesquisar'])) {
         }
     </script>
 
-
+    <script>
+        $("#7311400_0").click(function() {
+            setTimeout(
+            $(".toast").toast('show')
+            ,5000);
+        });
     </script>
 
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 </footer>
 
 </html>
